30a49d5b01a65222d3e68a4a3367a8eb376e6c6f
Jesse Shinol <jshinol1994@gmail.com>
2022-07-31 12:49:15 -0400

    hello

--- hello_world.py
+++ hello_world.py
@@ -13,4 +13,5 @@ fave_food1 = "sushi"
 fave_food2 = "pizza"
 print( "I love to eat {} and {}".format(fave_food1,fave_food2) ) # with .format()
 print( "I love to eat %s and %s" % (fave_food1,fave_food2) ) # with an f string
-
+#this line wont work in my python version because it was implemented after 
+# print(f"I Love to eat {fave_food1} and {fave_food2}")
